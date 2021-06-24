b20477a1605ce5c3818ba495e35def7b23b272d0
Vladimir <elcopro@gmail.com>
2021-06-24 17:57:59 +0300

    Modul Start

--- app/Http/Controllers/PostcardController.php
+++ app/Http/Controllers/PostcardController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\PostcardRequest;
 use App\Mail\OrderCreated;
 use App\Models\Order;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Mail;
 
 class PostcardController extends Controller
 {
+    /**
+     * @param PostcardRequest $request
+     * @return RedirectResponse
+     */
     public function create(PostcardRequest $request)
     {
         $order = Order::query()->create($request->all());
@@ -16,11 +24,18 @@ class PostcardController extends Controller
         return redirect()->action([PostcardController::class, 'order'], compact('order'));
     }
 
+    /**
+     * @return Application|Factory|View
+     */
     public function createOrder()
     {
         return view('create-order');
     }
 
+    /**
+     * @param Order $order
+     * @return Application|Factory|View
+     */
     public function order(Order $order)
     {
         return view('order', compact('order'));
