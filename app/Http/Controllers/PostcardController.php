a9ac13ca872698460b9653d0c94d015b1448ac4a
Vladimir <elcopro@gmail.com>
2021-06-24 14:40:41 +0300

    Add mail

--- app/Http/Controllers/PostcardController.php
+++ app/Http/Controllers/PostcardController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostcardRequest;
+use App\Mail\OrderCreated;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
 
 class PostcardController extends Controller
 {
     public function create(PostcardRequest $request)
     {
         $order = Order::query()->create($request->all());
+        Mail::to($order->email)->queue(new OrderCreated($order));
         return redirect()->action([PostcardController::class, 'order'], compact('order'));
     }
 
